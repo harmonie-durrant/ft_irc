d51906143103720cbaf1dd805bdada985c134ae7
renflo974 <froque@student.42perpignan.fr>
2024-12-10 12:33:03 +0100

    add privacy client attributes

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: froque <froque@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/10 12:28:42 by froque            #+#    #+#             */
+/*   Updated: 2024/12/10 12:28:46 by froque           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
@@ -6,6 +18,9 @@
 
 class Client {
 	private:
+		std::string	_nickname;
+		std::string	_username;
+		std::string	_fullname;
 		int			_fd;
 		int			_port;		//apparemment peu utile de recuperer le port, on verra si on garde ou pas
 		std::string _ip_addr;
