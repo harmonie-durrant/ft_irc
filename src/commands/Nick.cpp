59719a11739560f0b28b7a475d4bc10580e3187b
Harmonie Durrant <harmoniedurrant@gmail.com>
2024-12-10 17:46:03 +0100

    Delete: debug message

--- src/commands/Nick.cpp
+++ src/commands/Nick.cpp
@@ -10,7 +10,6 @@ void Nick::execute(Client* client, std::vector<std::string> args) {
 		client->send_response("ERR :Not enough parameters\r\n");
 		return;
 	}
-	std::cout << "TEST EEK" << args.size() << std::endl;
 	client->setNickname(args[1]);
 	client->send_response(":" + args[1] + " NICK " + args[1] + "\r\n");
 }
