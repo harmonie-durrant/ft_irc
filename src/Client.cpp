c8cb342992b99369f2f27b45d7b154ebc2fc2df7
Harmonie Durrant <harmoniedurrant@gmail.com>
2024-12-10 08:21:04 +0100

    Fix client variable declaration order - Client::_ip_addr created errors in my environment

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,9 +1,9 @@
 #include "Client.hpp"
 
-Client::Client() : _fd(0), _ip_addr(""), _port(0) {
+Client::Client() : _fd(0), _port(0), _ip_addr("") {
 }
 
-Client::Client(int fd, std::string ip_addr, int port) : _fd(fd), _ip_addr(ip_addr), _port(port) {
+Client::Client(int fd, std::string ip_addr, int port) : _fd(fd), _port(port), _ip_addr(ip_addr) {
 }
 
 Client::~Client() {
