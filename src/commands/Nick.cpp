3df3b774d989c72c922c7c6539b4c98403bb0087
renflo974 <froque@student.42perpignan.fr>
2024-12-16 13:28:29 +0100

    add define ERR_ and RPL_ numeric message

--- src/commands/Nick.cpp
+++ src/commands/Nick.cpp
@@ -1,4 +1,6 @@
 #include "Command.hpp"
+#include "numeric_error.hpp"
+#include "numeric_rpl.hpp"
 
 Nick::Nick(Server* server, bool auth) : Command(server, auth) {}
 
@@ -7,7 +9,7 @@ Nick::~Nick() {}
 void Nick::execute(Client* client, std::vector<std::string> args) {
 	if (args.size() < 2)
 	{
-		client->send_response(461, _server, client, args[0] + " :Not enough parameters");
+		client->send_response(ERR_NEEDMOREPARAMS, _server, client, args[0] + " :Not enough parameters");
 		return;
 	}
 	std::string oldnick = client->getNickname();
@@ -22,11 +24,11 @@ void Nick::execute(Client* client, std::vector<std::string> args) {
 		nickname = args[1];
 		if (args[1][0] == ' ')
 			nickname = nickname.substr(1);
-		client->send_response(433, _server, client, nickname + " " + nickname);
+		client->send_response(ERR_NICKNAMEINUSE, _server, client, nickname + " " + nickname);
 		return;
 	}
 	client->setNickname(nickname);
 	client->send_response(-1, _server, client, ":" + oldnick + " NICK " + client->getNickname());
 	if (!client->getUsername().empty() && !client->getFullname().empty() && !client->getHostname().empty())
-		client->send_response(001, _server, client, ":Welcome to the Internet Relay Network " + client->getNickname() + "!" + client->getUsername() + "@" + client->getHostname());
+		client->send_response(RPL_WELCOME, _server, client, ":Welcome to the Internet Relay Network " + client->getNickname() + "!" + client->getUsername() + "@" + client->getHostname());
 }
