00e920699c8015550595bad708979e0b8f838286
Harmonie Durrant <harmoniedurrant@gmail.com>
2024-12-18 13:07:12 +0100

    Add: ping error

--- src/commands/Ping.cpp
+++ src/commands/Ping.cpp
@@ -5,5 +5,10 @@ Ping::Ping(Server* server, bool auth) : Command(server, auth) {}
 Ping::~Ping() {}
 
 void Ping::execute(Client* client, std::vector<std::string> args) {
+	if (args.size() < 2)
+	{
+		client->send_response(ERR_NOORIGIN, _server, client, args[0] + " :No origin specified");
+		return;
+	}
 	client->send_response(-1, _server, client, "PONG " + args[1]);
 }
