05906c3e1fab711740ae94c83c96d848821971ff
Harmonie Durrant <harmoniedurrant@gmail.com>
2025-01-17 12:30:43 +0100

    Remove cap functionality (not needed)

--- src/commands/Cap.cpp
+++ src/commands/Cap.cpp
@@ -5,22 +5,7 @@ Cap::Cap(Server* server, bool auth) : Command(server, auth) {}
 Cap::~Cap() {}
 
 void Cap::execute(Client* client, std::vector<std::string> args) {
-	if (args.size() < 2)
-	{
-		client->send_response(ERR_NEEDMOREPARAMS, client, args[0] + " :Not enough parameters");
-		return;
-	}
-	if (args[1] == "LS")
-	{
-		client->send_response(-1, client, "CAP * LS :");
-		return;
-	}
-	if (args[1] == "REQ")
-	{
-		client->send_response(-1, client, "CAP * ACK :");
-		return;
-	}
-	if (args[1] == "END")
-		return;
-	client->send_response(ERR_INVALIDCAPCMD, client, args[1] + " :Invalid CAP subcommand");
+	(void) client;
+	(void) args;
+	// this is so that CAP LS doesn't send unknown command
 }
